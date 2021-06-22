04dc4c1ae7dab1bc87a53e949acaa95b10f3a5de
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-06-21 21:58:26 -0700

    idk

--- processvid.py
+++ processvid.py
@@ -1,7 +1,9 @@
 import cv2
+import time
 import numpy as np
 from pysndfx import AudioEffectsChain
 from mydia import Videos
+#processes effects on a numpy array in place
 fx = (
     AudioEffectsChain()
     .highshelf()
@@ -13,7 +15,7 @@ reader = Videos()
 
 # Call the 'read()' function to get the video tensor
 # which will be of shape (num_videos, num_frames, height, width, color_depth)
-video = reader.read("res/demovid_unprocessed")[0] # get video tensor
+video = reader.read("res/demovid_unprocessed.mp4")[0] # get video tensor
 """
 TODO: The Plan:
 Holy shit this is hard to think about
@@ -23,4 +25,21 @@ apply fx() to that array
 recombine to regular shape for a video
 save or play it back via opencv
 """
-red_tensor = video[] # this makes my head hurt, i dont know how to properly address the tensor
+for i in range(video.shape[2]):
+    for j in range(video.shape[1]):
+        red_tensor = video[..., i, j, 0].astype(np.float32) 
+        video[..., i, j, 0] = fx(red_tensor).astype(np.uint8)
+
+        green_tensor = video[..., i, j, 1] 
+        video[..., i, j, 1] = fx(green_tensor)
+
+        blue_tensor = video[..., i, j, 2] 
+        video[..., i, j, 2] = fx(blue_tensor)
+
+#play the processed video
+cv2.namedWindow("output")
+for f in video:
+    cv2.imshow(f)
+    time.sleep(1/24)
+cv2.waitKey(0)
+    
